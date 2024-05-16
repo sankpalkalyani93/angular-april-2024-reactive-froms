83ba872b03854f91049e075c56df8e0a4b0d77a1
kalyanisankpal <sankpalkalyani93@gmail.com>
2024-05-16 10:17:20 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'reactive-forms-demo';
+  registrationForm!: FormGroup;
+
+  constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(6)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required] 
+    });
+  }
+
+  onSubmit():void {
+    if(this.registrationForm.valid){
+      console.log(this.registrationForm.value);
+    }
+  }
+
 }
